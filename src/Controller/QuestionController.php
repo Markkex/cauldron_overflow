0214c8e7f267958a60a788c28b6fb1efb00e1f26
Markkex <avila.marco.dev@gmail.com>
2024-01-30 14:44:53 +0000

    add new libraries, add new views and templates

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,11 +1,12 @@
 <?php 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController {
+class QuestionController extends AbstractController{
     
     #[Route('/')]
     public function homepage() {
@@ -14,11 +15,17 @@ class QuestionController {
 
     #[Route('/questions/{slug}')]
     public function show($slug) {
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still ?',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
 
-        return new Response(sprintf(
-            'Future page to show the question "%s"',
-            ucwords(str_replace('-', ' ', $slug))
-        ));
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)), 
+            'answers' => $answers
+        ]);
     }
 
 
